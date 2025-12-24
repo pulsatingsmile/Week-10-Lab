e21690e69ffa3188986b833a12f19073d481cf9d
solidairity <synasokunketana@gmail.com>
2025-12-24 22:35:58 +0700

    first commit

--- S2 - PRACTICE/1 - START CODE/EXERCISE 5/js/script.js
+++ S2 - PRACTICE/1 - START CODE/EXERCISE 5/js/script.js
@@ -2,33 +2,73 @@
 // FUNCTIONS
 // ----------------------------------------------------------------------------
 
+let tasks = [];
+
+function displayTasks(task) {
+  const container = document.getElementById("taskList");
+  container.innerHTML = "";
+  for (let i = 0; i < task.length; i++) {
+    let taskDiv = document.createElement("div");
+    taskDiv.textContent = task[i].description;
+    taskDiv.style.padding = "10px";
+    taskDiv.style.marginBottom = "5px";
+    taskDiv.style.color = "white";
+    if (task[i].priority == 1) {
+      taskDiv.style.backgroundColor = "red";
+    } else {
+      taskDiv.style.backgroundColor = "grey";
+    }
+    container.appendChild(taskDiv);
+  }
+}
+
 function addItem() {
-  let newtask = {};
+  const descInput = document.getElementById("description");
+  const priInput = document.getElementById("priority");
 
-  const desc = document.getElementById("description");
-  newtask["description"] = desc.value;
+  let priorityValue = 0;
+  if (priInput.value === "High") {
+    priorityValue = 1;
+  } else {
+    priorityValue = 0;
+  }
 
-  const pri = document.getElementById("priority");
-  if(newtask["priority"] == "High"){
-    newtask["priority"] = 1;
-  }
-  else{
-    newtask["priority"] = 0;
-  }
+  let newtask = {
+    description: descInput.value,
+    priority: priorityValue,
+  };
 
   tasks.push(newtask);
 
-  desc.value = "";
+  descInput.value = "";
 
-  console.log(tasks);
+  displayTasks(tasks);
+}
+
+function showHigh() {
+  let importantTasks = tasks.filter((task) => task.priority === 1);
+  displayTasks(importantTasks);
+}
+
+function showLow() {
+  let notImportantTasks = tasks.filter((task) => task.priority === 0);
+  displayTasks(notImportantTasks);
+}
+
+function showAll() {
+  displayTasks(tasks);
 }
 
 // ----------------------------------------------------------------------------
 // MAIN
 // ----------------------------------------------------------------------------
 
-let tasks = [];
-
 // run the function addItem when you click on the button
 let addButton = document.getElementById("addButton");
 addButton.addEventListener("click", addItem);
+let btnFilterHigh = document.getElementById("btnFilterHigh");
+btnFilterHigh.addEventListener("click", showHigh);
+let btnFilterLow = document.getElementById("btnFilterLow");
+btnFilterLow.addEventListener("click", showLow);
+let btnFilterAll = document.getElementById("btnFilterAll");
+btnFilterAll.addEventListener("click", showAll);
